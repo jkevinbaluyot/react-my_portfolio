9d64dc0cab12f9f20ac092b9440a17d554026595
John Kevin Baluyot <johnkevin.baluyot@bitcontroltech.com>
2025-04-27 13:42:13 +0800

    Added banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import './Banner.scss';
 import ProfileImage from '../../assets/images/8bit-profile.png'; // Update the path to your image
 import Image from 'react-bootstrap/Image';
 
 const Banner = () => {
   return (
-    <div className="banner d-flex align-items-center justify-content-center flex-column flex-lg-row">
-      <div className='text-center text-lg-end mx-3'>
-        <h1>John Kevin Baluyot</h1>
-        <p>Your tagline or description goes here.</p>
+    <div className="banner section d-flex align-items-center justify-content-center flex-column flex-lg-row">
+      <div className='mx-3'>
+        <h1>John Kevin 
+          <span className='small d-block'> Baluyot</span>
+        </h1>
+        <p>I'm a web developer.</p>
       </div>
       <div className='mx-3'>
-        {/* Add your image here */}
         <Image src={ProfileImage} rounded/>
       </div>
     </div>
